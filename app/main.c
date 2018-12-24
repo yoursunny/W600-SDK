765183f6442c60d7b66a27481933a19604b8c3a6
Junxiao Shi <git@mail1.yoursunny.com>
2018-12-24 13:43:44 -0500

    app: create task from UserMain, don't block

--- app/main.c
+++ app/main.c
@@ -1,6 +1,16 @@
-#include "wm_include.h"
+#include <wm_include.h>
+
+static void
+UserTask(void* taskParam)
+{
+  while (true) {
+    tls_os_time_delay(1);
+  }
+}
 
 void
 UserMain()
 {
+  static OS_STK stack[2048];
+  tls_os_task_create(NULL, NULL, UserTask, NULL, (u8*)stack, sizeof(stack) / sizeof(stack[0]), 32, 0);
 }
